469170935d9bf40e76d3f56d37355cf6db4bb4c4
karenwang <kojenwang7@gmail.com>
2024-05-10 23:01:07 -0400

    Using for loop and if to merge strings

--- 1768. Merge Strings Alternately.py
+++ 1768. Merge Strings Alternately.py
@@ -2,7 +2,9 @@ class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         i = 0
         list = []
-        for i in range(min(len(word1), len(word2))):
-            list.append(word1[i])
-            list.append(word2[i])
+        for i in range(max(len(word1), len(word2))):
+            if i < len(word1):
+                list.append(word1[i])
+            if i < len(word2):
+                list.append(word2[i])
         return ''.join(list)
