e55ab77385ad40580a0e792d8b91b6fb44177455
karenwang <kojenwang7@gmail.com>
2024-06-06 23:08:21 -0400

    Move non-zero to the left iteratively

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[index] = nums[index], nums[i]
+                index += 1
